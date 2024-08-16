34b21a588d84ac1ef393dd1b7a18c9c22abfa441
Diego Cabrera <90268530+Diegcor@users.noreply.github.com>
2024-08-15 23:17:39 -0600

    timer utility header

---
+++ headers/timer.h
@@ -0,0 +1,10 @@
+#ifndef TIMER_FUNCTIONS
+#define TIMER_FUNCTIONS
+
+void setIn(bool val, bool& target, int millis);
+
+void setTrueIn(bool& flag, int millis);
+
+void setFalseIn(bool& flag, int millis);
+
+#endif
