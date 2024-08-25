f8b6a1cad8fb0c5d034664337d693df1d78b371d
Diego Cabrera <90268530+Diegcor@users.noreply.github.com>
2024-08-25 14:23:12 -0600

    CReated basic test program

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,20 @@
+#include <pigpio.h>
+#include <thread>
+
+using namespace std;
+
 int main(int argc, char* argv[]) {
-    //starts task threads
+    if (gpioInitialise() < 0)
+    {
+        return -1;
+    } else
+    {
+        gpioSetMode(2, PI_OUTPUT);
+        gpioWrite(2, 1);
+        this_thread::sleep_for(chrono::milliseconds(1000))
+        gpioWrite(2,0);
+        gpioTerminate();
+    }
 
-    
-
-
-
-    //loop until specified to stop
-
-
-
+    return 0;
 }
